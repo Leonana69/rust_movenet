bfae852f832d2d03419b88b0b695379a208b2c6c
leonana69 <guojun.chen@yale.edu>
2022-10-06 21:15:53 -0400

    replace tflite with tflitec, and use movenet instead of posenet

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::slice::from_raw_parts;
-use opencv::core::{flip};
+use opencv::core::{flip, Vec3b};
 use opencv::videoio::*;
 use opencv::{
 	prelude::*,
@@ -9,13 +8,15 @@ use opencv::{
 
 mod utils;
 use utils::*;
+use tflitec::interpreter::{Interpreter, Options};
 
 fn main() {
 	// load model and create interpreter
-	let mut interpreter = load_model("resource/posenet_mobilenet_v1_100_257x257_multi_kpt_stripped.tflite");
-	let input_index = interpreter.inputs().to_vec()[0];
-	let output_heatmap_index = interpreter.outputs().to_vec()[0];
-	let output_offset_index = interpreter.outputs().to_vec()[1];
+	let options = Options::default();
+	let path = format!("resource/lite-model_movenet_singlepose_lightning_tflite_int8_4.tflite");
+	let interpreter = Interpreter::with_model_path(&path, Some(options)).unwrap();
+	interpreter.allocate_tensors().expect("Allocate tensors [FAILED]");
+	// Resize input
 	
 	// open camera
 	let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY).unwrap(); // 0 is the default camera
@@ -30,24 +31,22 @@ fn main() {
 			// flip the image horizontally
 			let mut flipped = Mat::default();
 			flip(&frame, &mut flipped, 1).expect("flip [FAILED]");
+			// resize the image as a square, size is 
+			let resized_img = resize_with_padding(&flipped, [192, 192]);
 
-			let resized_img = resize_with_padding(&flipped, [257, 257]);
-
-			// get slice of buffer for fast copy
-			let input_buffer = interpreter.tensor_data_mut::<f32>(input_index).unwrap();
-			let data_buffer = unsafe { from_raw_parts(resized_img.data(), 257 * 257 * 3) };
-			for i in 0..257 * 257 * 3 {
-				// normalize pixel value to [-0.5, 0.5]
-				input_buffer[i] = data_buffer[i] as f32 / 127.5 - 1.0;
-			}
-			// the interpreter is quite slow
+			// turn Mat into Vec<u8>
+			let vec_2d: Vec<Vec<Vec3b>> = resized_img.to_vec_2d().unwrap();
+			let vec_1d: Vec<u8> = vec_2d.iter().flat_map(|v| v.iter().flat_map(|w| w.as_slice())).cloned().collect();
+			// set input (tensor0)
+			interpreter.copy(&vec_1d[..], 0).unwrap();
+			
+			// run interpreter
 			interpreter.invoke().expect("Invoke [FAILED]");
 
 			// get output
-			let output_heatmap: &[f32] = interpreter.tensor_data(output_heatmap_index).unwrap();
-			let output_offset: &[f32] = interpreter.tensor_data(output_offset_index).unwrap();
-			draw_keypoints(&mut flipped, output_heatmap, output_offset, 0.25);
-			imshow("PoseNet", &flipped).expect("imshow [ERROR]");
+			let output_tensor = interpreter.output(0).unwrap();
+			draw_keypoints(&mut flipped, output_tensor.data::<f32>(), 0.25);
+			imshow("MoveNet", &flipped).expect("imshow [ERROR]");
 		}
 		// keypress check
 		let key = wait_key(1).unwrap();
--- src/utils.rs
+++ src/utils.rs
@@ -2,11 +2,7 @@ use opencv::{
 	prelude::*,
 	imgproc::*,
 	core::*,
-	highgui::*,
 };
-
-use tflite::ops::builtin::*;
-use tflite::{FlatBufferModel, InterpreterBuilder, Interpreter};
 
 pub fn resize_with_padding(img: &Mat, new_shape: [i32;2]) -> Mat {
 	let img_shape = [img.cols(), img.rows()];
@@ -45,71 +41,31 @@ pub fn resize_with_padding(img: &Mat, new_shape: [i32;2]) -> Mat {
 	rslt
 }
 
-fn parse_keypoints(heatmap: &[f32], offset: &[f32], threshold: f32) -> Vec<Vec<f32>> {
-	let mut pose_kpts = vec![vec![0.0 as f32; 3]; 17];
-	for index in 0..17 {
-		let mut max_val = 0.0 as f32;
-		let mut max_row = 0;
-		let mut max_col = 0;
-		for r in 0..9 {
-			for c in 0..9 {
-				if heatmap[r * 9 * 17 + c * 17 + index] > max_val {
-					max_val = heatmap[r * 9 * 17 + c * 17 + index];
-					max_row = r;
-					max_col = c;
-				}
-			}
-		}
-
-		pose_kpts[index][0] = max_col as f32 / 8.0 * 257.0 + offset[max_row * 9 * 34 + max_col * 34 + index + 17];
-		pose_kpts[index][1] = max_row as f32 / 8.0 * 257.0 + offset[max_row * 9 * 34 + max_col * 34 + index];
-		if max_val > threshold {
-			pose_kpts[index][2] = 1.0;
-		}
-	}
-	pose_kpts
-}
-
-pub fn draw_keypoints(img: &mut Mat, heatmap: &[f32], offset: &[f32], threshold: f32) {
-	// heatmap: [9, 9, 17]
-	// offset: [9, 9, 34]
-	let pose_kpts = parse_keypoints(heatmap, offset, threshold);
-
-	let ratio: f32;
+pub fn draw_keypoints(img: &mut Mat, keypoints: &[f32], threshold: f32) {
+	// keypoints: [1, 17, 3]
+	let base: f32;
 	let pad_x: i32;
 	let pad_y: i32;
 	if img.rows() > img.cols() {
-		ratio = img.rows() as f32 / 257.0;
+		base = img.rows() as f32;
 		pad_x = (img.rows() - img.cols()) / 2;
 		pad_y = 0;
 	} else {
-		ratio = img.cols() as f32 / 257.0;
+		base = img.cols() as f32;
 		pad_x = 0;
 		pad_y = (img.cols() - img.rows()) / 2;
 	}
 
 	for index in 0..17 {
-		if pose_kpts[index][2] > 0.0 {
+		let y_ratio = keypoints[index * 3];
+		let x_ratio = keypoints[index * 3 + 1];
+		let confidence = keypoints[index * 3 + 2];
+		if confidence > threshold {
 			circle(img,
-				Point { x: (ratio * pose_kpts[index][0]) as i32 - pad_x, y: (ratio * pose_kpts[index][1]) as i32 - pad_y},
+				Point { x: (x_ratio * base) as i32 - pad_x, y: (y_ratio * base) as i32 - pad_y},
 				0,
 				Scalar::new(0.0, 255.0, 0.0, 0.0),
 				5, LINE_AA, 0).expect("Draw circle [FAILED]");
 		}
 	}
-}
-
-pub fn load_model(file_name: &str) -> Interpreter<BuiltinOpResolver> {
-	let model = FlatBufferModel::build_from_file(
-		file_name)
-		.expect("FlatBufferModel: load file [FAILED]");
-
-	let builder = InterpreterBuilder::new(
-		model,
-		BuiltinOpResolver::default()
-	).expect("InterpreterBuilder init [FAILED]");
-
-	let mut interpreter = builder.build().expect("Build interpreter [FAILED]");
-	interpreter.allocate_tensors().expect("Tensor allocation [FAILED]");
-	interpreter
 }
